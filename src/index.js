bad0f3d15351cff7f1c75cf0ba8322e5c1a3e2fd
Annu <54701665+AnnuSharma90@users.noreply.github.com>
2021-01-27 01:40:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import './index.css';
 //import App from './App';
-//import Final from './Final';
+import Final from './Final';
 //import App from './Hooks.js';
 //import Form from './Form';
 import Formh from "./Formh";
-import Inc from "./Inc";
+//import Inc from "./Inc";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Inc />
+		<Final />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
