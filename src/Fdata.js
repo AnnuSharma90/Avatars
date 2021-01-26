a3e0216e57f3eea719808e060d25e752f57ad316
Annu <54701665+AnnuSharma90@users.noreply.github.com>
2021-01-27 01:45:46 +0530

    render final

--- src/Fdata.js
+++ src/Fdata.js
@@ -1,26 +1,26 @@
 const Fdata = [
 	{
-		name:'John Doe', 
-		occupation:'Web Developer', 
-		image:'https://joeschmoe.io/api/v1/1'
+		name: "John Doe",
+		occupation: "Web Developer",
+		image: "https://joeschmoe.io/api/v1/1",
 	},
 
 	{
-		name:'Marcy',
-	 	occupation:'Front-End-Developer',
-	  	image:'https://joeschmoe.io/api/v1/2'
+		name: "Marcy",
+		occupation: "Front-End-Developer",
+		image: "https://joeschmoe.io/api/v1/2",
 	},
 
 	{
-		name:'Adrie',
-	 	occupation:'Database', 
-	 	image:'https://joeschmoe.io/api/v1/3'
+		name: "Adrie",
+		occupation: "Database",
+		image: "https://joeschmoe.io/api/v1/3",
 	},
 
 	{
-	 	name:'Wanny',
-	  	occupation:'Backend Developer',
-	   	image:'https://joeschmoe.io/api/v1/4'
-	}
-]
+		name: "Wanny",
+		occupation: "Backend Developer",
+		image: "https://joeschmoe.io/api/v1/4",
+	},
+];
 export default Fdata;
